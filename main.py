80b11a917ac97179e3e90f7516469ed905ddd904
operose123 <nfelix4@hawkmail.hccfl.edu>
2024-09-30 00:59:04 +0000

    Added variables and if statements.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 """
 This program calculates prices for custom house signs.
 """
+charge = 0.00
+minCharge = 35.00
+numChars = 18
+color = 'black'
+woodType = 'oak'
 
-# Declare and initialize variables here.
-# Charge for this sign.
-# Number of characters.
-# Color of characters.
-# Type of wood.
-
-# Write assignment and if statements here as appropriate.
-
-# Output Charge for this sign.
+if numChars > 5:
+  charge = minCharge + ((numChars - 5.00) * 4.00) 
+else:
+  charge = minCharge
+if color == 'gold':
+  charge = charge + 15.00
+if woodType == 'oak':
+  charge = charge + 20.00
+if charge < 35.00:
+  charge = 35.00
+  
 print("The charge for this sign is $" + str(charge))
